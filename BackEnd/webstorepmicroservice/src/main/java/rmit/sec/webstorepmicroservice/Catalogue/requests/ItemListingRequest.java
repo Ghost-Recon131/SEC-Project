7f3ba7a2e83dee9f88525afae5632899a6beef2e
Jingxuan Feng <henryfeng24@pm.me>
2022-10-03 17:53:46 +1100

    fixed incorrect lombok import & removed unnecessary value

--- BackEnd/webstorepmicroservice/src/main/java/rmit/sec/webstorepmicroservice/Catalogue/requests/ItemListingRequest.java
+++ BackEnd/webstorepmicroservice/src/main/java/rmit/sec/webstorepmicroservice/Catalogue/requests/ItemListingRequest.java
@@ -1,17 +1,16 @@
 package rmit.sec.webstorepmicroservice.Catalogue.requests;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
 @Getter
 @Setter
-@NoArgsConstructor
+@AllArgsConstructor
 public class ItemListingRequest {
     private String itemName;
     private String itemDescription;
-    private Boolean itemAvailable;
     private Double itemPrice;
     private Integer itemQuantity;
     private String itemImage;
