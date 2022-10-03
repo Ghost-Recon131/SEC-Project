706fdf2257f6f80fd11163ec00e033ddc451101b
Jingxuan Feng <henryfeng24@pm.me>
2022-10-03 17:53:01 +1100

    fixed incorrect lombok import

--- BackEnd/webstorepmicroservice/src/main/java/rmit/sec/webstorepmicroservice/Catalogue/requests/EditItemListingRequest.java
+++ BackEnd/webstorepmicroservice/src/main/java/rmit/sec/webstorepmicroservice/Catalogue/requests/EditItemListingRequest.java
@@ -1,13 +1,13 @@
 package rmit.sec.webstorepmicroservice.Catalogue.requests;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
 @Getter
 @Setter
-@NoArgsConstructor
+@AllArgsConstructor
 public class EditItemListingRequest {
     private Long itemID;
     private String itemName;
