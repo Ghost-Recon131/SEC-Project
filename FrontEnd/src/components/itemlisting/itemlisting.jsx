212deeb4193bb9fe2e3e5d91cb75f12141cdf5af
Jingxuan Feng <henryfeng24@pm.me>
2022-10-10 14:17:59 +1100

    improved UI for viewing item

--- FrontEnd/src/components/itemlisting/itemlisting.jsx
+++ FrontEnd/src/components/itemlisting/itemlisting.jsx
@@ -3,16 +3,19 @@ import cookie from "js-cookie";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate, Link, useParams } from "react-router-dom";
-import {data} from "autoprefixer";
-import {clientAESDecrypt, clientAESEncrypt} from "../security/EncryptionUtils";
+import { data } from "autoprefixer";
+import {
+  clientAESDecrypt,
+  clientAESEncrypt,
+} from "../security/EncryptionUtils";
 
 export default function Component() {
   const navigate = useNavigate();
-  const token = sessionStorage.getItem('jwt-token')
-  const sessionID = sessionStorage.getItem('sessionID')
-  const userID = sessionStorage.getItem('userID')
+  const token = sessionStorage.getItem("jwt-token");
+  const sessionID = sessionStorage.getItem("sessionID");
+  const userID = sessionStorage.getItem("userID");
 
-  var [itemListing, setItemListing] = useState({});
+  var [product, setProduct] = useState({});
   var [images, setImages] = useState([]);
   var queryParams = new URLSearchParams(window.location.search);
   var itemID = queryParams.get("itemID");
@@ -22,91 +25,114 @@ export default function Component() {
   useEffect(() => {
     // Get item details
     async function fetch1() {
-
       const data = {
-        "itemID": clientAESEncrypt(itemID),
-        "sellerID": "",
-        "category": ""
-      }
+        itemID: clientAESEncrypt(itemID),
+        sellerID: "",
+        category: "",
+      };
 
-      let item = await axios.post(getGlobalState("backendDomain") + "/api/catalogue/viewItem?sessionID=" + sessionID, data);
+      let item = await axios.post(
+        getGlobalState("backendDomain") +
+          "/api/catalogue/viewItem?sessionID=" +
+          sessionID,
+        data
+      );
       item = item.data;
 
       const decrypted = {
-        "itemID": clientAESDecrypt(item.itemID),
-        "sellerID": clientAESDecrypt(item.sellerID),
-        "itemName": clientAESDecrypt(item.itemName),
-        "itemDescription": clientAESDecrypt(item.itemDescription),
-        "itemAvailable": clientAESDecrypt(item.itemAvailable),
-        "itemPrice": clientAESDecrypt(item.itemPrice),
-        "itemQuantity": clientAESDecrypt(item.itemQuantity),
-        "itemImage": clientAESDecrypt(item.itemImage),
-        "itemCategory": clientAESDecrypt(item.itemCategory)
-      }
-      setItemListing(decrypted);
+        itemID: clientAESDecrypt(item.itemID),
+        sellerID: clientAESDecrypt(item.sellerID),
+        itemName: clientAESDecrypt(item.itemName),
+        itemDescription: clientAESDecrypt(item.itemDescription),
+        itemAvailable: clientAESDecrypt(item.itemAvailable),
+        itemPrice: clientAESDecrypt(item.itemPrice),
+        itemQuantity: clientAESDecrypt(item.itemQuantity),
+        itemImage: clientAESDecrypt(item.itemImage),
+        itemCategory: clientAESDecrypt(item.itemCategory),
+      };
+      setProduct(decrypted);
     }
     fetch1();
   }, []);
 
-
   // Set
-  function confirmDeleteListing(){
-    if(!token){
+  function confirmDeleteListing() {
+    if (!token) {
       const confirm = true;
       setConfirmDelete(confirm);
-    }else{
+    } else {
       setError("You must be signed in to modify a listing");
     }
   }
 
-
   // TODO: Fix here
   // Function to disable a listing
-  async function deleteListing(){
+  async function deleteListing() {
     const data = {
-      "itemID": clientAESEncrypt(itemID),
-      "sellerID": "",
-      "category": ""
-    }
+      itemID: clientAESEncrypt(itemID),
+      sellerID: "",
+      category: "",
+    };
 
-    let response = await axios.post(getGlobalState("backendDomain") + "/api/authorised/catalogue/?sessionID=" + sessionID, data);
+    let response = await axios.post(
+      getGlobalState("backendDomain") +
+        "/api/authorised/catalogue/?sessionID=" +
+        sessionID,
+      data
+    );
 
     // Go back to home after delete
     // navigate("/");
   }
 
-
   return (
-      <div className="bg-white text-black">
-        <h1 className="text-lg">{itemListing.listingTitle}</h1>
-        Catagory: {itemListing.itemCategory} <br />
-        Description: {itemListing.itemDescription} <br />
-        Price: ${itemListing.itemPrice} <br />
-        Remaning stock: {itemListing.itemQuantity} <br />
-
-        <img className="h-50 w-96 rounded-t-lg object-cover"
-        src={itemListing.itemImage}/>
-        <br />
-    
-        {/*If: logged-in user == creator of item listing, show below*/}
-        {userID === itemListing.sellerID? (
-            <div>
-              <button className="text-yellow-400" onClick={() => navigate("/editListing?id=" + itemListing.id)}>Edit Listing</button>
-              <br></br>
-              <button className="text-red-600 font-bold" onClick={() => confirmDeleteListing()}>Disable Listing</button>
-
-              {/* If: logged-in user == creator of item listing and confirmed to delete account */}
-              {confirmDelete === true? (
-                  <div>
-                    <button className="text-red-600 font-bold" onClick={() => deleteListing()}>CONFIRM DISABLE</button>
-                  </div>
-              ): (<div className="text-yellow-500 font-bold"></div>)
-              }
-            </div>
-
-        ): (<div className="text-yellow-500 font-bold"></div>)
-        }
-
+    <div className="bg-black text-white flex">
+      <img
+        className="h-50 w-96 rounded-lg object-cover mr-6"
+        src={product.itemImage}
+      />
+      <div className="w-6/12">
+        <h1 className="text-2xl mb-8">{product.itemName}</h1>
+        <p className="mb-4">Category: {product.itemCategory}</p>
+        <p className="mb-4">Description: {product.itemDescription}</p>
+        <p className="mb-4">Price: ${product.itemPrice}</p>
+        <p className="mb-4">Remaning stock: {product.itemQuantity}</p>
       </div>
-  ); // part of return bracket
+
+      {/*If: logged-in user == creator of item listing, show below*/}
+      {userID === product.sellerID ? (
+        <div>
+          <button
+            className="text-yellow-400"
+            onClick={() => navigate("/editListing?id=" + product.id)}
+          >
+            Edit Listing
+          </button>
+          <br></br>
+          <button
+            className="text-red-600 font-bold"
+            onClick={() => confirmDeleteListing()}
+          >
+            Disable Listing
+          </button>
+
+          {/* If: logged-in user == creator of item listing and confirmed to delete account */}
+          {confirmDelete === true ? (
+            <div>
+              <button
+                className="text-red-600 font-bold"
+                onClick={() => deleteListing()}
+              >
+                CONFIRM DISABLE
+              </button>
+            </div>
+          ) : (
+            <div className="text-yellow-500 font-bold"></div>
+          )}
+        </div>
+      ) : (
+        <div className="text-yellow-500 font-bold"></div>
+      )}
+    </div>
+  );
 }
