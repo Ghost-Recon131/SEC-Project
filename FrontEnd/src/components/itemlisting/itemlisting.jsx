98aac92656941ae11ab272613c8bc6344ace1dc8
Jingxuan Feng <henryfeng24@pm.me>
2022-10-10 12:12:57 +1100

    updating itemlisting & newlisting for new endpoints

--- FrontEnd/src/components/itemlisting/itemlisting.jsx
+++ FrontEnd/src/components/itemlisting/itemlisting.jsx
@@ -3,118 +3,108 @@ import cookie from "js-cookie";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate, Link, useParams } from "react-router-dom";
+import {data} from "autoprefixer";
+import {clientAESDecrypt, clientAESEncrypt} from "../security/EncryptionUtils";
 
 export default function Component() {
-  var navigate = useNavigate();
+  const navigate = useNavigate();
+  const token = sessionStorage.getItem('jwt-token')
+  const sessionID = sessionStorage.getItem('sessionID')
+  const userID = sessionStorage.getItem('userID')
+
   var [itemListing, setItemListing] = useState({});
   var [images, setImages] = useState([]);
   var queryParams = new URLSearchParams(window.location.search);
-  var id = queryParams.get("id");
-  var [user, setUser] = useState({});
+  var itemID = queryParams.get("itemID");
   var [confirmDelete, setConfirmDelete] = useState(false);
-
+  const [error, setError] = useState("");
 
   useEffect(() => {
-
-    if (cookie.get("user")) {
-      user = JSON.parse(cookie.get("user"));
-      setUser(user);
-    }
-
     // Get item details
     async function fetch1() {
-      var res1 = await axios.get("https://ji1fy3w7p2.execute-api.us-east-1.amazonaws.com/item-listing/api/viewListingByID?id=" + id);
-      setItemListing(res1.data);
+
+      const data = {
+        "itemID": clientAESEncrypt(itemID),
+        "sellerID": "",
+        "category": ""
+      }
+
+      let item = await axios.post(getGlobalState("backendDomain") + "/api/catalogue/viewItem?sessionID=" + sessionID, data);
+      item = item.data;
+
+      const decrypted = {
+        "itemID": clientAESDecrypt(item.itemID),
+        "sellerID": clientAESDecrypt(item.sellerID),
+        "itemName": clientAESDecrypt(item.itemName),
+        "itemDescription": clientAESDecrypt(item.itemDescription),
+        "itemAvailable": clientAESDecrypt(item.itemAvailable),
+        "itemPrice": clientAESDecrypt(item.itemPrice),
+        "itemQuantity": clientAESDecrypt(item.itemQuantity),
+        "itemImage": clientAESDecrypt(item.itemImage),
+        "itemCategory": clientAESDecrypt(item.itemCategory)
+      }
+      setItemListing(decrypted);
     }
     fetch1();
-
-    async function fetch2() {
-      var res2 = await axios.get("https://ji1fy3w7p2.execute-api.us-east-1.amazonaws.com/item-listing/api/getListingImageLinks/" + id);
-      setImages(res2.data);
-    }
-    fetch2();
-
   }, []);
 
 
   // Set
   function confirmDeleteListing(){
-    var confirm = true;
-    setConfirmDelete(confirm);
+    if(!token){
+      const confirm = true;
+      setConfirmDelete(confirm);
+    }else{
+      setError("You must be signed in to modify a listing");
+    }
   }
 
 
-  // Function to delete a listing
+  // TODO: Fix here
+  // Function to disable a listing
   async function deleteListing(){
-    await axios.delete("https://ji1fy3w7p2.execute-api.us-east-1.amazonaws.com/item-listing/api/deleteItemListing/" + id + "?userID=" + user.id);
+    const data = {
+      "itemID": clientAESEncrypt(itemID),
+      "sellerID": "",
+      "category": ""
+    }
+
+    let response = await axios.post(getGlobalState("backendDomain") + "/api/authorised/catalogue/?sessionID=" + sessionID, data);
 
     // Go back to home after delete
-    navigate("/");
+    // navigate("/");
   }
 
-
-  // axios POST for PayPal Checkout
-  async function checkOut() {
-    const domain = window.location.host;
-
-    // axios post
-    const bodyParameters = {
-      buyerID: user.id,
-      sellerID: itemListing.accountId,
-      itemListingID: itemListing.id,
-      price: itemListing.price,
-      currency: "AUD",
-      successURL: domain + "/transactionsuccess",
-      cancelURL: domain + "/transactionfail"
-    };
-
-    var status = await axios.post("https://i5lunowrqh.execute-api.us-east-1.amazonaws.com/transactions/api/Transactions/createPayment", bodyParameters);
-
-    console.log("POST Response: " + JSON.stringify(status))
-    console.log("redirect link: " + JSON.stringify(status.data))
-
-    window.open(status.data, '_blank');
-  }
 
   return (
       <div className="bg-white text-black">
         <h1 className="text-lg">{itemListing.listingTitle}</h1>
-        Condition: {itemListing.itemCondition} <br />
-        Description: {itemListing.description} <br />${itemListing.price} <br />
-        {images.map((image,index) => (
-            <div>
-              <img src={image} width="15%" height=""></img>
-            </div>
-        ))}
+        Catagory: {itemListing.itemCategory} <br />
+        Description: {itemListing.itemDescription} <br />
+        Price: ${itemListing.itemPrice} <br />
+        Remaning stock: {itemListing.itemQuantity} <br />
 
-
+        <img className="h-50 w-96 rounded-t-lg object-cover"
+        src={itemListing.itemImage}/>
+        <br />
+    
         {/*If: logged-in user == creator of item listing, show below*/}
-        {user.id === itemListing.accountId? (
+        {userID === itemListing.sellerID? (
             <div>
               <button className="text-yellow-400" onClick={() => navigate("/editListing?id=" + itemListing.id)}>Edit Listing</button>
-              <p className="text-orange-600">Warning! Deleting process is permanent!</p>
               <br></br>
-              <button className="text-red-600 font-bold" onClick={() => confirmDeleteListing()}>Delete Listing</button>
+              <button className="text-red-600 font-bold" onClick={() => confirmDeleteListing()}>Disable Listing</button>
 
               {/* If: logged-in user == creator of item listing and confirmed to delete account */}
               {confirmDelete === true? (
                   <div>
-                    <button className="text-red-600 font-bold" onClick={() => deleteListing()}>CONFIRM DELETE</button>
+                    <button className="text-red-600 font-bold" onClick={() => deleteListing()}>CONFIRM DISABLE</button>
                   </div>
               ): (<div className="text-yellow-500 font-bold"></div>)
               }
             </div>
 
         ): (<div className="text-yellow-500 font-bold"></div>)
-        }
-
-
-        {/*Checks that a user ID is present*/}
-        {user.id != null && user.id !== itemListing.accountId? (
-            <div>
-              <button className="text-blue-400 font-bold" onClick={() => checkOut()}>PayPal Checkout</button>
-            </div>
-        ): (<div></div>)
         }
 
       </div>
--- FrontEnd/src/components/itemlisting/newlisting.jsx
+++ FrontEnd/src/components/itemlisting/newlisting.jsx
@@ -5,46 +5,53 @@ import { useEffect, useState } from "react";
 import { useNavigate, Link, useParams } from "react-router-dom";
 
 export default function Component() {
-  var navigate = useNavigate();
-  const [imageName, setImageName] = useState("");
-  const [imageFile, setImageFile] = useState(null);
-  var [error, setError] = useState("");
-  var [formData, setFormData] = useState({
-    listingTitle: "",
-    price: "",
-    itemCondition: "",
-    description: "",
-  });
-  const [confirm, setConfirm] = useState(false);
+  const navigate = useNavigate();
+  const token = sessionStorage.getItem("jwt-token");
+  const sessionID = sessionStorage.getItem("sessionID");
 
-  var {listingTitle, price, itemCondition, description } = formData;
-  var [user, setUser] = useState({});
-
-
-  // Part of drop down menu for item condition
-  let itemConditions = [
-    { label: "BRAND NEW", value: "BRAND_NEW" },
-    { label: "OPENED", value: "OPENED" },
-    { label: "USED", value: "USED" },
-    {label: "DAMAGED", value: "DAMAGED"},
-    {label: "BROKEN", value: "BROKEN"}
-  ]
-
-  let [itemConditionDropDown, setItemConditionDropDown] = useState("UNDEFINED")
-
-  let handleItemConditionChange = (e) => {
-    setItemConditionDropDown(e.target.value)
-  }
-
-
+  // Check user is signed in
   useEffect(() => {
-    if (cookie.get("user")) {
-      user = JSON.parse(cookie.get("user"));
-      setUser(user);
-    } else {
+    if (!token) {
       navigate("/signin");
     }
   }, []);
+
+  // "itemID": "NULL",
+  // "itemName": itemName,
+  // "itemDescription": itemDescription,
+  // "itemAvailable": "NULL",
+  // "itemPrice": price,
+  // "itemQuantity": itemQuantity,
+  // "itemImage": "NULL",
+  // "category": "NULL"
+
+
+  // Setup variables to list new items
+  const {itemName, itemDescription, itemPrice, itemQuantity, itemImage, category} = formData;
+  const [error, setError] = useState("");
+  const [formData, setFormData] = useState({
+    itemName: "",
+    price: "",
+    itemCondition: "",
+    itemDescription: "",
+  });
+  const [confirm, setConfirm] = useState(false);
+
+  // Part of drop down menu for item condition
+  let itemCategory = [
+    { label: "ELECTRONICS", value: "ELECTRONICS" },
+    { label: "BOOKS", value: "BOOKS" },
+    { label: "CLOTHING", value: "CLOTHING" },
+    {label: "GAMES", value: "GAMES"},
+    {label: "FOOD", value: "FOOD"},
+    {label: "OTHER", value: "OTHER"}
+  ]
+  let [itemCategoryDropDown, setItemCategoryDropDown] = useState("UNDEFINED")
+
+  let handleItemConditionChange = (e) => {
+    setItemCategoryDropDown(e.target.value)
+  }
+
 
   function formInputs(event) {
     event.preventDefault();
@@ -52,60 +59,32 @@ export default function Component() {
     setFormData({ ...formData, [name]: value });
   }
 
-  function formImage(event) {
-    event.preventDefault();
-
-    let file = event.target.files[0];
-    const imageFile = new FormData();
-    imageFile.append("file", file);
-
-    setImageFile(imageFile);
-
-    setImageName(
-        event.target.value
-            .toString()
-            .substring(event.target.value.toString().lastIndexOf("\\") + 1)
-    );
-
-    // setImageFile(event.target.files[0]);
-  }
-
 
   async function formSubmit(event) {
     event.preventDefault();
     try {
-      setFormData({...formData, itemCondition: itemConditionDropDown});
+      setFormData({...formData, itemCondition: itemCategoryDropDown});
 
-      // Create listing
-      var res1 = await axios.post("https://ji1fy3w7p2.execute-api.us-east-1.amazonaws.com/item-listing/newitemlisting/" + user.id, formData);
-
-
-      // 2nd Post to upload image to S3
-      if(imageFile !== null){
-        var res2 = await axios.post("https://ji1fy3w7p2.execute-api.us-east-1.amazonaws.com/item-listing/api/addImageToListing/" + res1.data +
-            "?userId=" +
-            user.id +
-            // user.id +
-            "&filename=" +
-            imageName,
-            imageFile
-        );
+      const data = {
+        "itemID": "NULL",
+        "itemName": itemName,
+        "itemDescription": itemDescription,
+        "itemAvailable": "NULL",
+        "itemPrice": itemPrice,
+        "itemQuantity": itemQuantity,
+        "itemImage": "NULL",
+        "category": "NULL"
       }
 
-      // console.log(JSON.stringify(image.files[0]))
+      // Create listing
+      var res1 = await axios.post(getGlobalState("backendDomain") + "/api/authorised/catalogue/listItem?sessionID=" + sessionID, data);
 
+      //
       navigate("/");
     } catch (resError) {
       setError(resError.response.data.error);
     }
   }
-
-  function setConfirmPost(){
-    var confirm = true;
-    setFormData({...formData, itemCondition: itemConditionDropDown});
-    setConfirm(confirm);
-  }
-
 
   return (
       <form
@@ -118,7 +97,7 @@ export default function Component() {
             listingTitle
           </label>
           <input
-              value={listingTitle}
+              value={itemName}
               name="listingTitle"
               onChange={formInputs}
               className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
@@ -129,10 +108,10 @@ export default function Component() {
         </div>
         <div className="mb-6">
           <label className="block text-grey-darker text-sm font-bold mb-2">
-            price
+          itemPrice
           </label>
           <input
-              value={price}
+              value={itemPrice}
               name="price"
               onChange={formInputs}
               className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
@@ -150,7 +129,7 @@ export default function Component() {
           <select onChange={handleItemConditionChange}>
             <option value="UNDEFINED"> -- Select your item's condition -- </option>
             {
-              itemConditions.map((itemConditionDropDown) => <option value={itemConditionDropDown.value}>{itemConditionDropDown.label}</option>)
+              itemCategory.map((itemConditionDropDown) => <option value={itemConditionDropDown.value}>{itemConditionDropDown.label}</option>)
             }
           </select>
         </div>
@@ -160,7 +139,7 @@ export default function Component() {
             description
           </label>
           <input
-              value={description}
+              value={itemDescription}
               name="description"
               onChange={formInputs}
               className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
@@ -169,29 +148,13 @@ export default function Component() {
               required
           />
         </div>
-        <input
-            type="file"
-            id="image-input"
-            accept="image/jpeg, image/png, image/jpg"
-            onChange={formImage}
-        ></input>
 
 
-        <div className="mt-5 flex items-center justify-between">
-          <button className="text-blue-400 font-bold" onClick={() => setConfirmPost()}>Confirm</button>
-        </div>
-        {confirm === true? (
-            <div className="mt-5 flex items-center justify-between">
-              <button
-                  className="bg-blue-500 hover:bg-blue-dark text-white font-bold py-2 px-4 rounded"
-                  type="submit"
-              >
-                Create listing
-              </button>
-            </div>
-        ): (<div></div>)
-        }
-
+        <button
+            className="bg-blue-500 hover:bg-blue-dark text-white font-bold py-2 px-4 rounded"
+            type="submit">
+          Create listing
+        </button>
 
         <h1 className="mt-5 text-red-500">{error}</h1>
       </form>
