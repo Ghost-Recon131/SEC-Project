453cabdd7345205ede7e28c98eea3699ad60a721
Jingxuan Feng <henryfeng24@pm.me>
2022-10-12 10:41:27 +1100

    progress on cart

--- FrontEnd/src/components/transaction/cart.jsx
+++ FrontEnd/src/components/transaction/cart.jsx
@@ -1,11 +1,10 @@
-import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-import {clientAESDecrypt, clientAESEncrypt,} from "components/security/EncryptionUtils";
-import { getGlobalState } from "components/utils/globalState";
-import { XMarkIcon } from "@heroicons/react/24/outline";
 import {getItemDetails} from "../utils/getItemDetails";
-import {removeItemFromCart} from "../utils/shoppingCart";
+import {removeItemFromCart, removeItemsFromCart} from "../utils/shoppingCart";
+import { Dialog, Transition } from '@headlessui/react'
+import { Fragment} from 'react'
+import { XMarkIcon } from '@heroicons/react/24/outline'
 
 export default function Example() {
   const navigate = useNavigate();
@@ -66,88 +65,138 @@ export default function Example() {
   }
 
   // Removes an item entirely from the cart
-  function removeItem(itemID){
-    removeItemFromCart(itemID);
+  async function removeItem(itemID){
+    const userCart = localStorage.getItem("cart");
+    products = JSON.parse(userCart);
+    // console.log("Items in cart: ", JSON.stringify(products));
+    // console.log("ItemID sent to method: ", itemID);
+    removeItemsFromCart(itemID);
+    await getProductDetails();
   }
 
+  const [open, setOpen] = useState(true)
+
   return (
-      <div className="pointer-events-none flex justify-center">
-        <div className="flex h-full flex-col bg-white shadow-xl w-[1200px] rounded-lg">
-          <div className="flex-1 overflow-y-auto py-6 px-4 sm:px-6">
-            <div className="flex items-start justify-between">
-              <h1 className="text-lg font-medium text-gray-900">Shopping cart</h1>
-            </div>
-            <div className="mt-8">
-              <div className="flow-root">
-                <ul role="list" className="-my-6 divide-y divide-gray-200">
-                  {itemArray.map((item) => (
-                      <li key={item.itemID} className="flex py-6">
-                        <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                          <img
-                              src={item.itemImage}
-                              alt="Image failed to load"
-                              className="h-full w-full object-cover object-center"
-                          />
-                        </div>
-                        <div className="ml-4 flex flex-1 flex-col">
-                          <div>
-                            <div className="flex justify-between text-base font-medium text-gray-900">
-                              <h3>
-                                <a href={item.href}>{item.itemName}</a>
-                              </h3>
-                              <p className="ml-4">${item.itemPrice} per item</p>
-                            </div>
-                            <p className="mt-1 text-sm text-gray-500">
-                              {item.color}
-                            </p>
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={setOpen}>
+          <Transition.Child
+              as={Fragment}
+              enter="ease-in-out duration-500"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in-out duration-500"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-hidden">
+            <div className="absolute inset-0 overflow-hidden">
+              <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+                <Transition.Child
+                    as={Fragment}
+                    enter="transform transition ease-in-out duration-500 sm:duration-700"
+                    enterFrom="translate-x-full"
+                    enterTo="translate-x-0"
+                    leave="transform transition ease-in-out duration-500 sm:duration-700"
+                    leaveFrom="translate-x-0"
+                    leaveTo="translate-x-full"
+                >
+                  <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
+                    <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+                      <div className="flex-1 overflow-y-auto py-6 px-4 sm:px-6">
+                        <div className="flex items-start justify-between">
+                          <Dialog.Title className="text-lg font-medium text-gray-900">Shopping cart</Dialog.Title>
+                          <div className="ml-3 flex h-7 items-center">
+                            <button
+                                type="button"
+                                className="-m-2 p-2 text-gray-400 hover:text-gray-500"
+                                onClick={() => setOpen(false)}
+                            >
+                              <span className="sr-only">Close panel</span>
+                              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                            </button>
                           </div>
-                          <div className="flex flex-1 items-end justify-between text-sm">
-                            <p className="text-gray-500">Quantity: {item.cartQuantity}</p>
-                            <div className="flex">
-                              <button
-                                  onClick={removeItem(item.itemID)}
-                                  type="button"
-                                  className="font-medium text-indigo-600 hover:text-indigo-500">
-                                Remove
-                              </button>
-                            </div>
+                        </div>
+
+                        <div className="mt-8">
+                          <div className="flow-root">
+                            <ul role="list" className="-my-6 divide-y divide-gray-200">
+                              {itemArray.map((product) => (
+                                  <li key={product.id} className="flex py-6">
+                                    <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                                      <img
+                                          src={product.itemImage}
+                                          alt= "Failed to load image"
+                                          className="h-full w-full object-cover object-center"
+                                      />
+                                    </div>
+
+                                    <div className="ml-4 flex flex-1 flex-col">
+                                      <div>
+                                        <div className="flex justify-between text-base font-medium text-gray-900">
+                                          <h3>
+                                            <a href={product.href}>{product.itemName}</a>
+                                          </h3>
+                                          <p className="ml-4">{product.itemPrice}</p>
+                                        </div>
+                                        <p className="mt-1 text-sm text-gray-500">{product.itemCategory}</p>
+                                      </div>
+                                      <div className="flex flex-1 items-end justify-between text-sm">
+                                        <p className="text-gray-500">Quantity {product.cartQuantity}</p>
+
+                                        <div className="flex">
+                                          <button
+                                              type="button"
+                                              className="font-medium text-indigo-600 hover:text-indigo-500"
+                                              onClick={() => removeItem(product.itemID)}
+                                          >
+                                            Remove
+                                          </button>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </li>
+                              ))}
+                            </ul>
                           </div>
                         </div>
-                      </li>
-                  ))}
-                </ul>
+                      </div>
+
+                      <div className="border-t border-gray-200 py-6 px-4 sm:px-6">
+                        <div className="flex justify-between text-base font-medium text-gray-900">
+                          <p>Subtotal</p>
+                          <p>$262.00</p>
+                        </div>
+                        <p className="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
+                        <div className="mt-6">
+                          <a
+                              href="#"
+                              className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700">
+                            Checkout
+                          </a>
+                        </div>
+                        <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
+                          <p>
+                            or
+                            <button
+                                type="button"
+                                className="font-medium text-indigo-600 hover:text-indigo-500"
+                                onClick={() => continueShopping()}>
+                              Continue Shopping
+                              <span aria-hidden="true"> &rarr;</span>
+                            </button>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </Dialog.Panel>
+                </Transition.Child>
               </div>
             </div>
           </div>
-
-          <div className="border-t border-gray-200 py-6 px-4 sm:px-6">
-            <div className="flex justify-between text-base font-medium text-gray-900">
-              <p>Subtotal</p>
-              <p>${totalCost}</p>
-            </div>
-            <p className="mt-0.5 text-sm text-gray-500">
-              Shipping and taxes calculated at checkout.
-            </p>
-            <div className="mt-6">
-              <a
-                  href="#"
-                  className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700">
-                Checkout
-              </a>
-            </div>
-            <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
-              <p>
-                or {"  "}
-                <button
-                    type="button"
-                    className="font-medium text-indigo-600 hover:text-indigo-500"
-                    onClick={continueShopping}>
-                  Continue Shopping
-                </button>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-  );
+        </Dialog>
+      </Transition.Root>
+  )
 }
